1fd51d77a14ecc21cf846d0b6652c164f397914a
Akash Soni <akashsoni8285@gmail.com>
2019-09-02 20:31:49 +0530

    pop view using sheet

---
+++ pop view in swiftui.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Service-hit-swiftui
+//
+//  Created by Akash Soni on 02/09/19.
+//  Copyright © 2019 Akash Soni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var pop = false
+    var body: some View {
+        VStack{
+            Text("pop").sheet(isPresented: $pop) {
+                GroupView()
+            }.onTapGesture {
+                self.pop = true
+            }
+        }
+    }
+}
+struct GroupView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 3) {
+            Text("test")
+        }.frame(width:100,height: 180)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
